61969eca6d741fd3510e732e0a6ba8b389282333
Mellyday <mellysenover@gmail.com>
2021-02-19 00:27:32 +0700

    Delete Dead Code

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -94,35 +94,4 @@ public class CoffeeMachine {
 
         myCoffeeMachine.print();
     }
-
-    @SuppressWarnings("ConstantConditions")
-    public static void canMake(int cups, int water, int milk, int coffeeBeans) {
-        final int waterPerCup = 200, milkPerCup = 50, coffeeBeansPerCup = 15;
-
-        int maxCupsLimitedByWater = water / waterPerCup;
-        int maxCupsLimitedByMilk = milk / milkPerCup;
-        int maxCupsLimitedByCoffeeBeans = coffeeBeans / coffeeBeansPerCup;
-        int maxCups = Math.min(maxCupsLimitedByWater, Math.min(maxCupsLimitedByMilk,
-                maxCupsLimitedByCoffeeBeans));
-
-        if (cups == maxCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cups > maxCups) {
-            System.out.println("No, I can make only " + maxCups + " cup(s) of coffee");
-        } else if (cups < maxCups) {
-            System.out.println("Yes, I can make that amount of coffee (and even " +
-                    (maxCups - cups) + " more than that)");
-        }
-    }
-
-    /*
-    System.out.println("Write how many ml of water the coffee machine has:");
-        int water = Integer.parseInt(scan.nextLine());
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = Integer.parseInt(scan.nextLine());
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffeeBeans = Integer.parseInt(scan.nextLine());
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = Integer.parseInt(scan.nextLine());
-     */
 }
